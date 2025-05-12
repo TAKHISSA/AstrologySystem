bcefade2762e5afef3e0c4d2c0b1ce115fe5bb4b
Emre Altundag <116040406+TAKHISSA@users.noreply.github.com>
2025-05-12 10:42:40 +0300

    Update background image for light theme

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   const backgroundStyle = {
     backgroundImage: darkMode
       ? 'url("https://images.pexels.com/photos/2469122/pexels-photo-2469122.jpeg")'
-      : 'url("https://images.pexels.com/photos/1252890/pexels-photo-1252890.jpg")',
+      : 'url("https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
