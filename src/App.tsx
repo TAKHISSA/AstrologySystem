75d94d34d77a2c05fdc387ed00b168d07b194f64
Emre Altundag <116040406+TAKHISSA@users.noreply.github.com>
2025-05-12 16:34:19 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="min-h-screen transition-colors duration-200" style={backgroundStyle}>
       {/* Overlay katmanı */}
-      <div className="absolute inset-0 bg-white/30 dark:bg-black/30 backdrop-blur-sm z-0" />
+      <div className="absolute inset-0 bg-white/30 dark:bg-black/30  z-0" />
 
       <div className="min-h-screen relative z-10 bg-gray-50/90 dark:bg-black/80 py-12 px-4 backdrop-blur-sm">
         <div className="max-w-6xl mx-auto">
