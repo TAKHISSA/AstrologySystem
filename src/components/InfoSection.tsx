1498a336879bd4d8a86f6b2a996501f40d73fdc9
Emre Altundag <116040406+TAKHISSA@users.noreply.github.com>
2025-05-15 17:19:57 +0300

    Updated InfoSection.tsx

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -16,7 +16,7 @@ const photos: PhotoInfo[] = [
   },
   {
     id: 2,
-    url: "https://i.imgur.com/KA0PyEd.jpeg",
+    url: "https://i.imgur.com/Aqy4y8h.jpegg",
     title: "Ücret Bilgisi"
   },
   {
